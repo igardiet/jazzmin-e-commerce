6e2a247a051689ca5e52a2156a25296725304fbf
Iñaki García <igardiet@gmail.com>
2023-02-22 10:53:14 +0100

    File update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './library/commerce';
 import { Products, Navbar } from './components/export';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+    
+    const fetchProducts = async() => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+    console.log(products)
     return (
         <div>
             <Navbar />
