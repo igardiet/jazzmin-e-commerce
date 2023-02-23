7382fe7c319b034b744792451b979c3d75ead0a9
Iñaki García <igardiet@gmail.com>
2023-02-23 15:57:55 +0100

    Review

--- src/environment/CartEnv.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CartEnv = () => {
-  return <div>CartEnv</div>;
-};
-
-export default CartEnv;
--- src/environment/SidebarEnv.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SidebarEnv = () => {
-  return <div>SidebarEnv</div>;
-};
-
-export default SidebarEnv;
