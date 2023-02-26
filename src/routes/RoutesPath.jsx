765939a8960b232375312c0e9ddb8bd4063c5279
Iñaki García <igardiet@gmail.com>
2023-02-26 19:12:08 +0100

    Wishlist.jsx file added to routes path

--- src/routes/RoutesPath.jsx
+++ src/routes/RoutesPath.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home';
 import Checkout from '../pages/Checkout';
+import Wishlist from '../pages/Wishlist';
 import Navbar from '../components/navbar/Navbar';
 
 
@@ -13,6 +14,7 @@ export const RoutesPath = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/checkout' element={<Checkout />} />
+                <Route path='/wishlist' element={<Wishlist />} />
                 <Route path='/*' element={<Navigate to ='/' />} />
             </Routes>
         </BrowserRouter>
