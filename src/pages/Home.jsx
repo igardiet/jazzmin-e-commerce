4c2a37d268dab4e2854318ded96a1d41b206a8a2
Iñaki García <igardiet@gmail.com>
2023-02-27 14:36:29 +0100

    Navbar and Footer adition. Home.css linked

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ProductContainer from '../components/container/ProductContainer'
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../../src/components/footer/Footer'
+import '../../src/pages/Home.css'
 
 const Home = () => {
   return (
     <div>
+    <Navbar />
     <ProductContainer />
+    <Footer />
     </div>
   )
 }
