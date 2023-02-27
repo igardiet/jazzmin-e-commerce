7f275bfa911b3a2558344f3ad8422dee95ab204d
Iñaki García <igardiet@gmail.com>
2023-02-27 14:34:11 +0100

    Cart list

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+const Cart = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Cart
