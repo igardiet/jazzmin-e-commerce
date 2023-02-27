d312859906c785f70877f1ae35397c4762cb04a6
Iñaki García <igardiet@gmail.com>
2023-02-27 14:32:24 +0100

    caps and code order update

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -9,14 +9,12 @@ const Product = ({ id, name, description, price, image }) => {
     // style="width: 18rem;"
     <div className="card" key={id}>
       <img src={image} className="card-img-top synthImage" alt="Synth" />
-      <div className="card-body">
+      <div className="cardBody">
         <h5 className="card-title">{name}</h5>
         <h5>{price}</h5>
-        <p className="card-text">
-          {description}
-        </p>
-        <Link to={'/'}><img src={addToCart} alt="Add to cart" className="addToCart" /></Link>
-        <Link to={'/'}><img src={addToWishList} alt="Wishlist" className="addToWishList" /></Link>
+        <p className="card-text">{description}</p>
+        <Link to={'/cart'}><img src={addToCart} alt="Add to cart" className="addToCart" /></Link>
+        <Link to={'/wishlist'}><img src={addToWishList} alt="Wishlist" className="addToWishList" /></Link>
       </div>
     </div>
   );
