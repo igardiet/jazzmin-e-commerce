d264ee3baa40729552e1582d66c42db5c4ee7b45
Iñaki García <igardiet@gmail.com>
2023-02-27 14:25:39 +0100

    Footer jsx and styles creation

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>2023</p>
+      <h5>© Jazzmin Synthesizers</h5>
+      <p className="card-text">
+        <small>App powered by React</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
