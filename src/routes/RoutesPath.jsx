d6466f4e0db69951f5c716510b3801c381817746
Iñaki García <igardiet@gmail.com>
2023-02-27 14:38:09 +0100

    Navbar uninported

--- src/routes/RoutesPath.jsx
+++ src/routes/RoutesPath.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home';
-import Checkout from '../pages/Checkout';
+import Cart from '../pages/Cart';
 import Wishlist from '../pages/Wishlist';
-import Navbar from '../components/navbar/Navbar';
 
 
 export const RoutesPath = () => {
   return (
     <div>
         <BrowserRouter>
-        <Navbar />
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/checkout' element={<Checkout />} />
-                <Route path='/wishlist' element={<Wishlist />} />
                 <Route path='/*' element={<Navigate to ='/' />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/wishlist' element={<Wishlist />} />
             </Routes>
         </BrowserRouter>
     </div>
