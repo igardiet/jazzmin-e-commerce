613565d463dde39515149232c382390412ab61ba
Iñaki García <igardiet@gmail.com>
2023-03-05 22:31:44 +0100

    quotes and € removed from product"s price

--- src/assets/db/db.js
+++ src/assets/db/db.js
@@ -10,7 +10,7 @@ const dataBase = [
         id: 1,
         name: 'Mavis',
         description: 'Mavis is a build-it-yourself analog synthesizer kit that delivers the huge depth of sound Moog is known for in a compact and feature-packed instrument.',
-        price: '€375',
+        price: 375,
         image: image
     },
 
@@ -18,7 +18,7 @@ const dataBase = [
         id: 2,
         name: 'Subsequent 25',
         description: 'Subsequent 25 is a 2-note paraphonic analog synthesizer that melds the hands-on analog soul of classic Moog instruments with the convenience and workflow of a modern sound-design machine. Moog’s most compact keyboard synthesizer, the Subsequent 25 delivers all of the rich sonic density that Moog synthesizers are known for.',
-        price: '€1349',
+        price: 1349,
         image: image2
     },
 
@@ -26,7 +26,7 @@ const dataBase = [
         id: 3,
         name: 'Minimoog Model D',
         description: 'Built to last a lifetime, each new Minimoog Model D delivers the revered quality, undefinable feel, and authentic sound of a pristine vintage Moog.',
-        price: '€5999',
+        price: 5999,
         image: image3
     },
 
@@ -34,7 +34,7 @@ const dataBase = [
         id: 4,
         name: 'Subsequent 37',
         description: 'The Subsequent 37® is a (2-note) paraphonic analog synthesizer that builds upon the award-winning design of the ultra-powerful Sub 37 Tribute Edition. Its control panel is home to 40 knobs and 74 switches, placing a vast array of analog sound-design tools and onboard sequencing options immediately at your command.',
-        price: '€1959',
+        price: 1959,
         image: image4
     },
 
@@ -42,7 +42,7 @@ const dataBase = [
         id: 5,
         name: 'Moog One',
         description: 'Moog One® is the ultimate Moog synthesizer - a tri-timbral, polyphonic, analog dream-synth designed to inspire imagination, stimulate creativity, and unlock portals to vast new realms of sonic potential.',
-        price: '€8099',
+        price: 8099,
         image: image5
     },
 
@@ -50,7 +50,7 @@ const dataBase = [
         id: 6,
         name: 'Subharmonicon',
         description: 'Subharmonicon is a semi-modular polyrhythmic analog synthesizer that employs a 6-tone sound engine and multi-layered clock generator to explore the world of subharmonics, polyrhythms, and the unique relationships they create.',
-        price: '€699',
+        price: 699,
         image: image6
     },
   
