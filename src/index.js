623e849fe45a9642d383974d31bcda857629dbc1
Iñaki García <igardiet@gmail.com>
2023-03-05 22:40:34 +0100

    strict mode off and App import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RoutesPath } from "./routes/RoutesPath";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RoutesPath />
-  </React.StrictMode>
+  // <React.StrictMode>
+      <App />
+  /* </React.StrictMode> */
 );
