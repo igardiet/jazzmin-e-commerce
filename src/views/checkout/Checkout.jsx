ce1e9f5f01b7435050c98f3be92717f7472d0e79
Iñaki García <igardiet@gmail.com>
2023-03-12 23:35:11 +0100

    word wishlist switched for checkout in file name

---
+++ src/views/checkout/Checkout.jsx
@@ -0,0 +1,12 @@
+import "./Checkout.css";
+
+export const Checkout = () => {
+
+  return (
+    <div>
+      <div className="checkoutStyles">
+        <span>CHECKOUT</span>
+      </div>
+    </div>
+  );
+};
