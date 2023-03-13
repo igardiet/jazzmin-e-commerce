d2081c47d48ccd4e2be1b5ef1882271d7a60ece4
Iñaki García <igardiet@gmail.com>
2023-03-13 18:40:55 +0100

    code cleaned and formatedd

--- src/views/cart/Cart.jsx
+++ src/views/cart/Cart.jsx
@@ -1,53 +1,62 @@
-import "./Cart.css";
 import { useContext, useEffect, useState } from "react";
-import { ProductContext } from "../../context/ProductContext";
 import { CartContext } from "../../context/CartContext";
+import "./Cart.css";
+// import { ProductContext } from "../../context/ProductContext";
 
-
-const Cart = () => {
-  const { products, setProducts } = useContext(ProductContext);
+export const Cart = () => {
+  // const { products, setProducts } = useContext(ProductContext);
   const { cart, setCart } = useContext(CartContext);
 
   const deleteProduct = (id) => {
     let newCart = [...cart];
-        newCart.forEach((data, key) => {
-          if (data.id === id) {
-            newCart.splice(key, 1)
-          }
-        })
-        setCart(newCart)
-      localStorage.setItem("cart", JSON.stringify(newCart));
+    newCart.forEach((data, key) => {
+      if (data.id === id) {
+        newCart.splice(key, 1);
+      }
+    });
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
   };
 
   const [total, setTotal] = useState(0);
 
   const calculateTotal = () => {
-    const total = products.reduce((prev, curr) => {
+    const total = cart.reduce((prev, curr) => {
       return prev + curr.price;
     }, 0);
 
     setTotal(total);
   };
   useEffect(() => {
-    if (!products.length) return;
     calculateTotal();
-  }, [products]);
-
+  }, [cart]);
   return (
-      <div className="cartStyles">
-        {cart.map((cartItem, index) => {
-          return (
-            <div key={`${cartItem.id}-${index}`}>
+    <div className="cartStyles">
+      {cart.map((cartItem, index) => {
+        return (
+          
+          <div className="card cardCart" key={`${cartItem.id}-${index}`}>
+            <div className="card-header">
               <p>{cartItem.name}</p>
-              <p>{cartItem.price}</p>
-              <button onClick={() => deleteProduct(cartItem.id)}>Delete</button>
             </div>
-          );
-        })}
-        <div>
-          <p>Total:{total}€</p>
-        </div>
-      </div>
+            <div className="card-body">
+              <h5 className="card-title">
+                {" "}
+                <p>€ {cartItem.price}</p>
+              </h5>
+
+              <button
+                onClick={() => deleteProduct(cartItem.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <div>{total !== 0 && <p>Total: € {total}</p>}</div>
+      <div>{total !== 0 && <button className="btn btn-success" style={{marginBottom: '3rem'}}>Buy</button>}</div>
+    </div>
   );
 };
-export default Cart;
