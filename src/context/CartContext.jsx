c206f3da540a3f5ef61e5112f4b980ede2a5d047
Iñaki García <igardiet@gmail.com>
2023-03-13 18:44:11 +0100

    code cleaned and formatted

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,13 +2,12 @@ import React, { createContext, useEffect, useState } from "react";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  
-    const [cart, setCart] = useState([]);
-    
-    useEffect(() => {
-      const cartList = JSON.parse(localStorage.getItem('cart')) || []
-    setCart(cartList)
-  }, [])
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const cartList = JSON.parse(localStorage.getItem("cart")) || [];
+    setCart(cartList);
+  }, []);
 
   return (
     <CartContext.Provider
